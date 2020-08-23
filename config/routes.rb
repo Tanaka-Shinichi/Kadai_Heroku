d188af74e37aa237ee3339b2e65ff689c1d05928
Tanaka_Shinichi <brilliantworld710@gmail.com>
2020-08-23 17:29:29 +0000

    Kadai Rails05

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'pages#index'
   get 'pages/index'
   
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
